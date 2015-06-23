f83d0f26b9143f1e819d79e4df693fa05d83b752
qicosmos <qicosmos@163.com>
2015-06-23 22:21:18 +0800

    boost换成std

--- examples/http_server/main.cpp
+++ examples/http_server/main.cpp
@@ -1,10 +1,11 @@
 
 
 #include <cinatra/http_server.hpp>
+#include <thread>
 
 int main()
 {
-	cinatra::HTTPServer s(boost::thread::hardware_concurrency());
+	cinatra::HTTPServer s(std::thread::hardware_concurrency());
 	s.listen("0.0.0.0", "http").run();
 
 	return 0;
