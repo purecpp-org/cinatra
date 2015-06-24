07500b8ad569f98084ed8a512e587d6258d1dbe2
Jos <gnu.crazier@gmail.com>
2015-06-24 23:14:19 +0800

    添加http response status

--- include/cinatra/response.hpp
+++ include/cinatra/response.hpp
@@ -114,18 +114,28 @@ namespace cinatra
 		{
 			std::map<int, std::string>codes_detail_ =
 			{
+				{ 100, "HTTP/1.1 100 Continue\r\n" },
+				{ 101, "HTTP/1.1 101 Switching Protocols\r\n" },
+				{ 102, "HTTP/1.1 102 Processing\r\n" },
+			
 				{ 200, "HTTP/1.1 200 OK\r\n" },
 				{ 201, "HTTP/1.1 201 Created\r\n" },
 				{ 202, "HTTP/1.1 202 Accepted\r\n" },
+				{ 203, "HTTP/1.1 203 Non-Authoritative Information\r\n" },
 				{ 204, "HTTP/1.1 204 No Content\r\n" },
+				{ 205, "HTTP/1.1 205 Reset Content\r\n" },
+				{ 206, "HTTP/1.1 206 Partial Content\r\n" },
+				{ 207, "HTTP/1.1 207 Muti Status\r\n" },
 
 				{ 300, "HTTP/1.1 300 Multiple Choices\r\n" },
 				{ 301, "HTTP/1.1 301 Moved Permanently\r\n" },
 				{ 302, "HTTP/1.1 302 Moved Temporarily\r\n" },
+				{ 303, "HTTP/1.1 303 See Other\r\n" },
 				{ 304, "HTTP/1.1 304 Not Modified\r\n" },
 
 				{ 400, "HTTP/1.1 400 Bad Request\r\n" },
 				{ 401, "HTTP/1.1 401 Unauthorized\r\n" },
+				{ 402, "HTTP/1.1 402 Payment Required\r\n" },
 				{ 403, "HTTP/1.1 403 Forbidden\r\n" },
 				{ 404, "HTTP/1.1 404 Not Found\r\n" },
 
