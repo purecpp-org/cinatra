3b3556f91748e0dbeebf415a99de28de714a5b30
Jos <gnu.crazier@gmail.com>
2015-06-29 23:39:12 +0800

    添加测试

--- test/request_parser/request_parser.cpp
+++ test/request_parser/request_parser.cpp
@@ -22,7 +22,9 @@ BOOST_AUTO_TEST_CASE(http_parser_header_test)
 	RequestParser parser;
 	std::string content = "POST /test HTTP/1.1\r\n";
 	BOOST_REQUIRE(parser.parse(content.begin(), content.end()) != RequestParser::bad);
-	content = "key1: val1\r\nkey1:val11\r\nkey2:val2\r\n\r\n";
+	
+	/** FIXME: 测试失败，key:val -> key: val则成功 */
+	content = "key1:val1\r\nkey1: val11\r\nkey2: val2\r\n\r\n";
 	BOOST_REQUIRE(parser.parse(content.begin(), content.end()) == RequestParser::good);
 	auto req = parser.get_request();
 	BOOST_CHECK(req.raw_url == "/test");
@@ -62,4 +64,35 @@ BOOST_AUTO_TEST_CASE(http_parser_kv_body_test)
 	BOOST_CHECK(std::string(req.raw_body.begin(), req.raw_body.end()) == "k=v");
 	BOOST_REQUIRE(req.body.has_key("k"));
 	BOOST_CHECK(req.body.get_val("k") == "v");
+}
+
+BOOST_AUTO_TEST_CASE(http_parser_map_test)
+{
+	RequestParser p;
+	std::string content = "GET /test?k1=v1&k2=v2&k3=v3 HTTP/1.1\r\nke1: va1\r\nke2: va2\r\n"
+		"Content-Length: 17\r\n\r\nkey1=val1&key2=val2&key3=val3";
+	const size_t size = content.size();
+
+	/** FIXME: 测试失败 */
+	size_t pos = 0;
+	for(; pos < size - 1; ++pos)
+		BOOST_CHECK(p.parse(content.begin() + pos, content.begin() + pos + 1) == RequestParser::indeterminate);
+	BOOST_CHECK(p.parse(content.begin() + pos, content.begin() + pos + 1) == RequestParser::good);
+
+	auto req = p.get_request();
+	BOOST_CHECK(req.method == "GET");
+	BOOST_CHECK(req.path == "/test");
+	BOOST_CHECK(req.raw_url == "/test?k1=v1&k2=v2&k3=v3");
+
+	BOOST_CHECK(req.query.get_val("k1") == "v1");
+	BOOST_CHECK(req.query.get_val("k2") == "v2");
+	BOOST_CHECK(req.query.get_val("k3") == "v3");
+
+	BOOST_CHECK(req.header.get_val("ke1") == "va1");
+	BOOST_CHECK(req.header.get_val("ke2") == "va2");
+	BOOST_CHECK(req.header.get_val("ke3") == "va3");
+	
+	BOOST_CHECK(req.body.get_val("key1") == "val1");
+	BOOST_CHECK(req.body.get_val("key2") == "val2");
+	BOOST_CHECK(req.body.get_val("key3") == "val3");
 }
