f4cffb77f81cbfe9ecc17debd73852631ded18dc
qiyu <qiyu@kingsoft.com>
2015-07-06 17:35:46 +0800

    调整http_router的代码。

--- include/cinatra/http_router.hpp
+++ include/cinatra/http_router.hpp
@@ -7,44 +7,10 @@
 #include "lexical_cast.hpp"
 #include "string_utils.hpp"
 #include "tuple_utils.hpp"
+#include "token_parser.hpp"
 
 class HttpRouter
 {
-	class token_parser
-	{
-		std::vector<std::string> m_v;
-	public:
-
-		token_parser(std::string& s, char seperator)
-		{
-			m_v = StringUtil::split(s, seperator);
-		}
-
-	public:
-		template<typename RequestedType>
-		typename std::decay<RequestedType>::type get()
-		{
-			if (m_v.empty())
-				throw std::invalid_argument("unexpected end of input");
-
-			try
-			{
-				typedef typename std::decay<RequestedType>::type result_type;
-
-				auto it = m_v.begin();
-				result_type result = lexical_cast<typename std::decay<result_type>::type>(*it);
-				m_v.erase(it);
-				return result;
-			}
-			catch (std::exception& e)
-			{
-				throw std::invalid_argument(std::string("invalid argument: ") + e.what());
-			}
-		}
-
-		bool empty(){ return m_v.empty(); }
-	};
-
 	typedef std::function<void(token_parser &)> invoker_function;
 
 	std::map<std::string, invoker_function> map_invokers;
