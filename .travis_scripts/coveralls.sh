b7fa8d1851cdf07a0a96ea1ae59e8801ac06fcc8
Jos <gnu.crazier@gmail.com>
2015-07-08 23:46:40 +0800

    add travis scripts

---
+++ .travis_scripts/coveralls.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Note that this only works if the tests were built using --coverage for
+# compile and link flags!
+if [ "$CXX" == "g++" ];
+then
+	sudo pip install cpp-coveralls
+	cd build
+	coveralls -r ../include --gcov-options '\-lp'
+fi
