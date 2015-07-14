6b807a3bf0249438919287ece2022ca461e271dd
qiyu <qiyu@kingsoft.com>
2015-07-14 10:48:49 +0800

    支持成员函数

--- include/cinatra/aop.hpp
+++ include/cinatra/aop.hpp
@@ -23,38 +23,49 @@ struct AOP : NonCopyable
 	}
 
 	template<typename T>
-	typename std::enable_if<has_member_before<T, Args...>::value&&has_member_after<T, Args...>::value>::type Invoke(Args&&... args, T&& aspect)
+	typename std::enable_if<has_member_before<T, Args...>::value&&has_member_after<T, Args...>::value>::type invoke(bool& result, Args&&... args, T&& aspect)
 	{
-		bool r = aspect.before(std::forward<Args>(args)...);//核心逻辑之前的切面逻辑
-		if (r)
+		result = aspect.before(std::forward<Args>(args)...);//核心逻辑之前的切面逻辑
+		if (result)
 		{
-			m_func(std::forward<Args>(args)...);//核心逻辑
+			result = m_func(std::forward<Args>(args)...);//核心逻辑
 			aspect.after(std::forward<Args>(args)...);//核心逻辑之后的切面逻辑
 		}
 	}
 
 	template<typename T>
-	typename std::enable_if<has_member_before<T, Args...>::value&&!has_member_after<T, Args...>::value>::type Invoke(Args&&... args, T&& aspect)
+	typename std::enable_if<has_member_before<T, Args...>::value&&!has_member_after<T, Args...>::value>::type invoke(bool& result, Args&&... args, T&& aspect)
 	{
-		bool r = aspect.before(std::forward<Args>(args)...);//核心逻辑之前的切面逻辑
-		if (r)
-			m_func(std::forward<Args>(args)...);//核心逻辑
+		result = aspect.before(std::forward<Args>(args)...);//核心逻辑之前的切面逻辑
+		if (result)
+			result = m_func(std::forward<Args>(args)...);//核心逻辑
 	}
 
 	template<typename T>
-	typename std::enable_if<!has_member_before<T, Args...>::value&&has_member_after<T, Args...>::value>::type Invoke(Args&&... args, T&& aspect)
+	typename std::enable_if<!has_member_before<T, Args...>::value&&has_member_after<T, Args...>::value>::type invoke(bool& result, Args&&... args, T&& aspect)
 	{
-		m_func(std::forward<Args>(args)...);//核心逻辑
+		result = m_func(std::forward<Args>(args)...);//核心逻辑
 		aspect.after(std::forward<Args>(args)...);//核心逻辑之后的切面逻辑
 	}
 
-	template<typename Head, typename... Tail>
-	void Invoke(Args&&... args, Head&&headAspect, Tail&&... tailAspect)
+	template<typename T, typename Self>
+	typename std::enable_if<has_member_before<T, Args...>::value&&has_member_after<T, Args...>::value>::type invoke_member(bool& result, Self* self, Args&&... args, T&& aspect)
 	{
-		headAspect.before(std::forward<Args>(args)...);
-		Invoke(std::forward<Args>(args)..., std::forward<Tail>(tailAspect)...);
-		headAspect.after(std::forward<Args>(args)...);
+		result = aspect.before(std::forward<Args>(args)...);//核心逻辑之前的切面逻辑
+		if (result)
+		{
+			result = (*self.*m_func)(std::forward<Args>(args)...);//核心逻辑
+			aspect.after(std::forward<Args>(args)...);//核心逻辑之后的切面逻辑
+		}
 	}
+
+	//template<typename Head, typename... Tail>
+	//void invoke(bool& result, Args&&... args, Head&&headAspect, Tail&&... tailAspect)
+	//{
+	//	headAspect.before(std::forward<Args>(args)...);
+	//	invoke(result, std::forward<Args>(args)..., std::forward<Tail>(tailAspect)...);
+	//	headAspect.after(std::forward<Args>(args)...);
+	//}
 
 private:
 	Func m_func; //被织入的函数
@@ -63,8 +74,15 @@ template<typename T> using identity_t = T;
 
 //AOP的辅助函数，简化调用
 template<typename... AP, typename... Args, typename Func>
-void Invoke(Func&&f, Args&&... args)
+typename std::enable_if<!std::is_member_function_pointer<Func>::value>::type invoke(bool& result, Func&&f, Args&&... args)
 {
 	AOP<Func, Args...> asp(std::forward<Func>(f));
-	asp.Invoke(std::forward<Args>(args)..., identity_t<AP>()...);
+	asp.invoke(result, std::forward<Args>(args)..., identity_t<AP>()...);
+}
+
+template<typename... AP, typename... Args, typename Func, typename Self>
+typename std::enable_if<std::is_member_function_pointer<Func>::value>::type invoke(bool& result, Func&&f, Self* self, Args&&... args)
+{
+	AOP<Func, Args...> asp(std::forward<Func>(f));
+	asp.invoke_member(result, self, std::forward<Args>(args)..., identity_t<AP>()...);
 }
