042872ec1263639be96d14e8a578b0ce12a92902
qiyu <qiyu@kingsoft.com>
2015-07-27 09:23:50 +0800

    修改支持C++11

--- include/cinatra/utils.hpp
+++ include/cinatra/utils.hpp
@@ -95,12 +95,38 @@ namespace cinatra
 
 	class NcaseMultiMap
 	{
+		struct NcaseHash
+		{
+			size_t operator()(const std::string& key) const
+			{
+				std::size_t seed = 0;
+				std::locale locale;
+
+				for (auto c : key)
+				{
+					boost::hash_combine(seed, std::toupper(c, locale));
+				}
+
+				return seed;
+			}
+		};
+
+		struct IsKeyEqu
+		{
+			bool operator()(const std::string& l, const std::string& r) const
+			{
+				return boost::iequals(l, r);
+			}
+		};
+		using map_t = boost::unordered_multimap<std::string, std::string, NcaseHash, IsKeyEqu>;
 	public:
-		auto begin() const {
+		map_t::iterator begin()
+		{
 			return map_.begin();
 		}
 		
-		auto end() const {
+		map_t::iterator end()
+		{
 			return map_.end();
 		}
 
@@ -173,29 +199,7 @@ namespace cinatra
 			return map_.find("Connection") != map_.end();
 		}
 	private:
-		struct NcaseHash
-		{
-			size_t operator()(const std::string& key) const
-			{
-				std::size_t seed = 0;
-				std::locale locale;
-
-				for (auto c : key)
-				{
-					boost::hash_combine(seed, std::toupper(c, locale));
-				}
-
-				return seed;
-			}
-		};
-
-		struct IsKeyEqu
-		{
-			bool operator()(const std::string& l, const std::string& r) const
-			{
-				return boost::iequals(l, r);
-			}
-		};
+		
 		boost::unordered_multimap<std::string, std::string, NcaseHash, IsKeyEqu> map_;
 	};
 
