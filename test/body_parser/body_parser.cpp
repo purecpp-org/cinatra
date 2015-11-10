ce7ff514517bc67611c37f0ad3415bd86ef61075
Jos <gnu.crazier@gmail.com>
2015-11-10 19:16:35 +0800

    fix test

--- test/body_parser/body_parser.cpp
+++ test/body_parser/body_parser.cpp
@@ -6,6 +6,6 @@ using namespace cinatra;
 BOOST_AUTO_TEST_CASE(urlencoded_body_parser_test)
 {
     CaseMap cm = urlencoded_body_parser("k%261=v%3d1&k%262=v%3d2");
-    BOOST_CHECK(cm["k%261"] == "v%3d1");
-    BOOST_CHECK(cm["k%262"] == "v%3d2");
+    BOOST_CHECK(cm["k&1"] == "v=1");
+    BOOST_CHECK(cm["k&2"] == "v=2");
 }
