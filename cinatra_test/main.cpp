e6b6bdd84397bf8ec47969bb0aeefbf5217bd800
网事如风 <67541967@qq.com>
2017-01-20 16:28:59 +0800

    添加session

--- cinatra_test/main.cpp
+++ cinatra_test/main.cpp
@@ -3,6 +3,7 @@
 
 #include <cinatra/cinatra.h>
 #include <cinatra/middleware/cookies.hpp>
+#include <cinatra/middleware/session.hpp>
 
 
 struct CheckLoginAspect
@@ -21,40 +22,52 @@ struct CheckLoginAspect
 
 int main()
 {
-	cinatra::cinatra<CheckLoginAspect, cinatra::cookies> app;
+	cinatra::cinatra<CheckLoginAspect, cinatra::cookies, cinatra::session> app;
 
-	app.route("/", [](cinatra::response& res)
+// 	app.route("/", [](cinatra::response& res)
+// 	{
+// 		res.response_text("hhhhhhhhhhhhhhhhhh");
+// 	});
+// 	app.route("/test/:name", [](std::string name, cinatra::response& res)
+// 	{
+// 		res.response_text(name);
+// 	});
+// 
+// 
+// 	app.route("/show_cookie", [](cinatra::response& res, cinatra::context_container& ctx)
+// 	{
+// 		std::string cookie_str;
+// 		auto& cookie_ctx = ctx.get_req_ctx<cinatra::cookies>();
+// 		for (auto it : cookie_ctx.get())
+// 		{
+// 			cookie_str += it.first + ": " + it.second + "\n";
+// 		}
+// 		res.response_text(cookie_str);
+// 	});
+// 
+// 	app.route("/add_cookie", [](cinatra::response& res, cinatra::context_container& ctx)
+// 	{
+// 		auto& cookie_ctx = ctx.get_req_ctx<cinatra::cookies>();
+// 		cinatra::cookies::cookie_t cookie;
+// 		cookie.add("foo", "bar");
+// 		cookie.set_http_only(true);
+// 		cookie_ctx.add_cookie(cookie);
+// 
+// 		res.response_text("over");
+// 	});
+
+	app.route("/set_session", [](cinatra::response& res, cinatra::context_container& ctx)
 	{
-		res.response_text("hhhhhhhhhhhhhhhhhh");
+		auto s = ctx.get_req_ctx<cinatra::session>();
+		s.add("test", std::string("hello"));
+		res.response_text("OK");
 	});
-	app.route("/test/:name", [](std::string name, cinatra::response& res)
+
+	app.route("/show_session", [](cinatra::response& res, cinatra::context_container& ctx)
 	{
-		res.response_text(name);
+		auto s = ctx.get_req_ctx<cinatra::session>();
+		res.response_text(s.get<std::string>("test"));
 	});
-
-
-	app.route("/show_cookie", [](cinatra::response& res, cinatra::context_container& ctx)
-	{
-		std::string cookie_str;
-		auto& cookie_ctx = ctx.get_req_ctx<cinatra::cookies>();
-		for (auto it : cookie_ctx.get())
-		{
-			cookie_str += it.first + ": " + it.second + "\n";
-		}
-		res.response_text(cookie_str);
-	});
-
-	app.route("/add_cookie", [](cinatra::response& res, cinatra::context_container& ctx)
-	{
-		auto& cookie_ctx = ctx.get_req_ctx<cinatra::cookies>();
-		cinatra::cookies::cookie_t cookie;
-		cookie.add("foo", "bar");
-		cookie.set_http_only(true);
-		cookie_ctx.add_cookie(cookie);
-
-		res.response_text("over");
-	});
-
 
 	app.listen("127.0.0.1", "8080");
 	app.run();
---
+++ libcinatra/cinatra/middleware/session.hpp
@@ -0,0 +1,206 @@
+
+#pragma once
+
+#include <cinatra_http/request.h>
+#include <cinatra_http/response.h>
+#include <cinatra/context_container.hpp>
+#include <cinatra/middleware/cookies.hpp>
+
+#include <boost/any.hpp>
+#include <boost/uuid/uuid.hpp>
+#include <boost/uuid/uuid_io.hpp>
+#include <boost/uuid/uuid_generators.hpp>
+
+#include <unordered_map>
+#include <string>
+#include <thread>
+#include <atomic>
+#include <mutex>
+
+
+
+namespace cinatra
+{
+	class session
+	{
+	public:
+		void before(request const& req, response& res, context_container& ctx)
+		{
+			ctx.add_req_ctx(context_t(sessions_, ctx));
+		}
+
+		void after(request const& req, response& res, context_container& ctx)
+		{
+
+		}
+
+		struct session_map_t
+		{
+			session_map_t()
+				:last_used_time(std::time(nullptr))
+			{}
+			std::time_t last_used_time;
+			std::unordered_map<std::string, boost::any> m;
+		};
+
+		class context_t
+		{
+		public:
+			context_t(std::unordered_map<std::string, session_map_t>& sessions,context_container& ctx)
+				:sessions_(sessions), ctx_(ctx)
+			{
+
+			}
+
+			template<typename T>
+			T& get(std::string const& key)
+			{
+				auto sp = get_session_map_with_session_id();
+				if (!sp)
+				{
+					throw std::invalid_argument("key " + key + " not found");
+				}
+
+				auto it = sp->m.find(key);
+				if (it == sp->m.end())
+				{
+					throw std::invalid_argument("key " + key + " not found");
+				}
+
+				return boost::any_cast<T&>(it->second);
+			}
+
+			template<typename T>
+			void add(std::string const& key, T const& val)
+			{
+				auto sp = get_session_map_with_add();
+				sp->m.emplace(key, val);
+			}
+
+			bool has(std::string const& key)
+			{
+				auto sp = get_session_map_with_session_id();
+				if (!sp)
+				{
+					return false;
+				}
+
+				return sp->m.find(key) != sp->m.end();
+			}
+
+			bool del(std::string const& key)
+			{
+				auto sp = get_session_map_with_session_id();
+				if (!sp)
+				{
+					return false;
+				}
+
+				auto it = sp->m.find(key);
+				if (it == sp->m.end())
+				{
+					return false;
+				}
+
+				sp->m.erase(it);
+				return true;
+			}
+		private:
+			std::string get_session_id()
+			{
+				auto& cookie = ctx_.get_req_ctx<cookies>();
+				return cookie.get("CSESSIONID");
+			}
+
+			session_map_t* get_session_map()
+			{
+				return session_map_ptr_;
+			}
+
+			session_map_t* get_session_map_with_session_id()
+			{
+				if (session_map_ptr_)
+				{
+					return session_map_ptr_;
+				}
+				if (session_map_ptr_not_found_)
+				{
+					return nullptr;
+				}
+
+				auto& cookie = ctx_.get_req_ctx<cookies>();
+				auto session_id = cookie.get("CSESSIONID");
+				if (session_id.empty())
+				{
+					session_map_ptr_not_found_ = true;
+					return nullptr;
+				}
+
+				auto it = sessions_.find(session_id);
+				if (it == sessions_.end())
+				{
+					session_map_ptr_not_found_ = true;
+					return nullptr;
+				}
+
+				session_map_ptr_ = &it->second;
+
+				return session_map_ptr_;
+			}
+
+			inline void itoh(int c, char& out1, char& out2)
+			{
+				unsigned char hexchars[] = "0123456789ABCDEF";
+				out1 = hexchars[c >> 4];
+				out2 = hexchars[c & 15];
+			}
+
+			session_map_t* get_session_map_with_add()
+			{
+				if (session_map_ptr_)
+				{
+					return session_map_ptr_;
+				}
+
+				auto& cookie = ctx_.get_req_ctx<cookies>();
+				auto session_id = cookie.get("CSESSIONID");
+				if (session_id.empty())
+				{
+					boost::uuids::uuid u = boost::uuids::random_generator()();
+					for (auto c : u)
+					{
+						char out1, out2;
+						itoh(c, out1, out2);
+						session_id.push_back(out1);
+						session_id.push_back(out2);
+					}
+
+					cookies::cookie_t c;
+					c.add("CSESSIONID", session_id);
+					cookie.add_cookie(c);
+				}
+
+// 				sessions_.emplace(session_id_, session_map_t());
+				session_map_ptr_ = &sessions_[session_id];
+				return session_map_ptr_;
+			}
+
+
+			context_container& ctx_;
+			session_map_t* session_map_ptr_ = nullptr;
+			bool session_map_ptr_not_found_ = false;
+
+			std::unordered_map<std::string, session_map_t>& sessions_;
+		};
+	private:
+		std::mutex mtx_;
+		std::unordered_map<std::string, session_map_t> sessions_;
+	};
+
+
+
+
+
+
+
+}
