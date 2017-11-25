2c08dd16d6284ed9c295fe03a4854bcf23001ee8
qicosmos <qicosmos@163.com>
2017-11-25 10:28:51 +0800

    update

--- cinatra_test/main.cpp
+++ cinatra_test/main.cpp
@@ -77,7 +77,7 @@ int main()
     });
 
     person p;
-    app.register_handler<GET>("/get_id", &person::get_id, p, mylog{});
+    app.register_handler<GET>("/get_id", &person::get_id, person{}, mylog{});
     app.register_handler<GET>("/get_ip", &person::get_ip, p, mylog{});
     app.register_handler<GET>("/test_json", &person::get_json, p);
 
