1b34344f936e5b36adfb49072ff8b8704b2610cd
qicosmos <qicosmos@163.com>
2017-11-27 14:51:51 +0800

    update

--- cinatra_test/main.cpp
+++ cinatra_test/main.cpp
@@ -81,6 +81,51 @@ int main()
     app.register_handler<GET>("/get_ip", &person::get_ip, p, mylog{});
     app.register_handler<GET>("/test_json", &person::get_json, p);
 
+    app.register_handler<GET>("/ws_echo", [](cinatra::request const& req, cinatra::response& res)
+    {
+        auto client_key = cinatra::websocket::websocket_connection::is_websocket_handshake(req);
+        if (client_key.empty())
+        {
+            res.response_text("Not a websocket handshake");
+            return;
+        }
+
+        cinatra::websocket::ws_config_t cfg =
+        {
+            // on_start
+            [](cinatra::websocket::ws_conn_ptr_t conn)
+            {
+                static const std::string hello = "Hello websocket!";
+                conn->async_send_msg(hello, cinatra::websocket::TEXT, [](boost::system::error_code const& ec)
+                {
+                    if (ec)
+                    {
+                        std::cout << "Send websocket hello failed: " << ec.message() << std::endl;
+                    }
+                });
+            },
+            // on_message
+            [](cinatra::websocket::ws_conn_ptr_t conn, boost::string_ref msg, cinatra::websocket::opcode_t opc)
+            {
+                auto back_msg = std::make_shared<std::string>(msg.to_string());
+                conn->async_send_msg(*back_msg, opc, [back_msg](boost::system::error_code const& ec)
+                {
+                    if (ec)
+                    {
+                        std::cout << "Send websocket hello failed: " << ec.message() << std::endl;
+                    }
+                });
+            },
+            nullptr, nullptr, nullptr,
+            // on_error
+            [](boost::system::error_code const& ec)
+            {
+                std::cout << "upgrade to websocket failed:" << ec.message() << std::endl;
+            }
+        };
+        cinatra::websocket::websocket_connection::upgrade_to_websocket(req, res, client_key, cfg);
+    });
+
 	app.set_static_path("./static");
 	app.listen("0.0.0.0", "8080");
 	app.run();
